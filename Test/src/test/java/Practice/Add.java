b1603602938136f5a71753f60d55d8dbfdfe0b13
Admin <suryaprakash.kumar@outlook.com>
2025-07-28 17:23:19 +0530

    commit

---
+++ Test/src/test/java/Practice/Add.java
@@ -0,0 +1,12 @@
+package Practice;
+
+public class Add {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+	}
+
+}
